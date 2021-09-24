2023f0950f5f9732f577f25c9ac96e1c82855b93
overult01 <overult01@gmail.com>
2021-09-24 16:07:17 +0900

    로그인 페이지 구현

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+//import user from './user_reducer';
+// import comment from './comment_reducer';
+
+const rootReducer = combineReducers({
+    // 기능이 많아질 수록 늘어남
+    //user
+    // comment
+    
+})
+
+// export default 로 가져와서 다른 파일으로 내보내기시 다른 이름으로 불러오기가 가능하다.
+export default rootReducer;
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginPage() {
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        // setEmail을 이용해서 state를 변경할 수 있음.
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        // setEmail을 이용해서 state를 변경할 수 있음.
+        setPassword(event.currentTarget.value)
+    }    
+
     return (
-        <div>
-            LoginPage 로그인페이지
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+
+        <form style={{ display: "flex", FlexDirection: "column"}}>
+            <label>Email</label>
+            <input type="email" value={Email} onChange={onEmailHandler} />
+            <label>Password</label>
+            <input type="password" value={Password} onChange={onPasswordHandler} />
+            <br />
+            <button>
+                Login
+            </button>
+        </form>
+            
+
         </div>
     )
 }

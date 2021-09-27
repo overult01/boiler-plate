cfdcc455e53d91977e67965b9945cea18084a594
overult01 <overult01@gmail.com>
2021-09-27 18:59:40 +0900

    클라이언트 페이지에서 로그인시 콘솔에서 에러

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+/* eslint-disable no-unused-vars */
 import {
     LOGIN_USER
 } from './types';
@@ -7,7 +8,7 @@ import {
 export function loginUser(dataToSubmit) {
     const request = axios.post('/api/users/login', dataToSubmit)
         .then(response => response.data )
-
+/* eslint-disable no-unused-vars */
     return {
         type: "LOGIN_USER",
         payload: request
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+//import axios from 'axios';
 import {
     LOGIN_USER
 } from '../_actions/types';
